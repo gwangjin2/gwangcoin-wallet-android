61525ddadefbe681a43d7148e2ca7f348f85f1bf
Seungho Shin <frenzygale@gmail.com>
2014-08-18 07:22:26 +0900

    wallet_ltc -> wallet_swc

--- wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.java
+++ wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.java
@@ -95,7 +95,7 @@ import de.schildbach.wallet.util.CrashReporter;
 import de.schildbach.wallet.util.GenericUtils;
 import de.schildbach.wallet.util.ThrottlingWalletChangeListener;
 import de.schildbach.wallet.util.WalletUtils;
-import de.schildbach.wallet_ltc.R;
+import de.schildbach.wallet_swc.R;
 
 /**
  * @author Andreas Schildbach, Litecoin Dev Team
--- wallet/src/de/schildbach/wallet/ui/ScannerView.java
+++ wallet/src/de/schildbach/wallet/ui/ScannerView.java
@@ -35,7 +35,7 @@ import android.view.View;
 
 import com.google.zxing.ResultPoint;
 
-import de.schildbach.wallet_ltc.R;
+import de.schildbach.wallet_swc.R;
 
 /**
  * @author Andreas Schildbach, Litecoin Dev Team
