bd4abfcebf5d66d3b1b9872b5db0f8ca7238efed
yezune <yezune@gmail.com>
2014-09-02 18:02:56 +0900

    update suwoncoin-wallet version url

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -103,8 +103,9 @@ public class Constants
     public static final String ANDROID_WEBMARKET_APP_URL = "https://play.google.com/store/apps/details?id=%s";
     public static final String BB_WEBMARKET_APP_URL = "http://appworld.blackberry.com/webstore/content/46329888";
 
-	public static final String VERSION_URL = "http://wallet.schildbach.de/version";
-	public static final int HTTP_TIMEOUT_MS = 15 * (int) DateUtils.SECOND_IN_MILLIS;
+    //public static final String VERSION_URL = "http://wallet.schildbach.de/version";
+    public static final String VERSION_URL = "http://www.suwoncoin.org/version";
+    public static final int HTTP_TIMEOUT_MS = 15 * (int) DateUtils.SECOND_IN_MILLIS;
 
 	public static final String PREFS_KEY_LAST_VERSION = "last_version";
 	public static final String PREFS_KEY_LAST_USED = "last_used";
